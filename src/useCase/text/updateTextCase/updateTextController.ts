9fbcadd82c9ce71847aa9cb3bddc0282ddb94e3b
Igor Goncalo <igorlabakig@gmail.com>
2023-11-27 12:21:21 +0000

    e

--- src/useCase/text/updateTextCase/updateTextController.ts
+++ src/useCase/text/updateTextCase/updateTextController.ts
@@ -1,9 +1,8 @@
-import { UpdateTextCase } from "./updateTextCase";
-import { IUpdateTextParams } from "../../../repository/ITextRepository";
 import { Response, Request } from "express";
-import { PrismaTextRepository } from "../../../repository/inPrisma/prismaTextRepository";
+import { UpdateTextCase } from "./updateTextCase";
 import { prismaClient } from "../../../service/prisma";
 import { validateInput } from "../../../util/validateInput";
+import { PrismaTextRepository } from "../../../repository/inPrisma/prismaTextRepository";
 
 class UpdateTextController {
   async handle(req: Request, resp: Response) {
@@ -16,14 +15,17 @@ class UpdateTextController {
     const prismaTextRepository = new PrismaTextRepository(prismaClient);
     const updateTextsCase = new UpdateTextCase(prismaTextRepository);
 
-    const updateText = await updateTextsCase.execute( {
-      textId,
-      data: {
-        area, text, position, titulo
-      }
-    } );
-
-    return resp.json(updateText);
+    try {
+      const updateText = await updateTextsCase.execute( {
+        textId,
+        data: {
+          area, text, position, titulo
+        }
+      });
+      return resp.json(updateText);
+    } catch (error) {
+      return resp.status(400).json({ error: error.message });
+    }
   }
 }
 
