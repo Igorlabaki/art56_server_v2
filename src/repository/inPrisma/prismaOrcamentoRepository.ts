176e532cbab2067c9d23a4ccea265418699e1001
Igor Goncalo <igorlabakig@gmail.com>
2024-10-04 17:27:57 +0100

    s

--- src/repository/inPrisma/prismaOrcamentoRepository.ts
+++ src/repository/inPrisma/prismaOrcamentoRepository.ts
@@ -127,18 +127,18 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
     });
   }
 
-  async trafegoCount({year}: MonthCountParams): Promise<any> {
+  async trafegoCount({ year }: MonthCountParams): Promise<any> {
     const trafegoCounts = await this.prisma.orcamento.groupBy({
       by: ["trafegoCanal"],
       _count: {
         trafegoCanal: true,
       },
-      where:{
-        dataInicio:{
+      where: {
+        dataInicio: {
           gte: new Date(year ? year : new Date().getFullYear()),
           lt: new Date(year ? year : new Date().getFullYear(), 12, 31),
-        }
-      }
+        },
+      },
     });
 
     const orcamentoCount = trafegoCounts.reduce(
@@ -171,15 +171,15 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
     return trafegoData;
   }
 
-  async monthCount({year}: MonthCountParams): Promise<any> {
+  async monthCount({ year }: MonthCountParams): Promise<any> {
     const orcamentos = await this.prisma.orcamento.findMany({
       where: {
         aprovadoAr756: true,
         aprovadoCliente: true,
-        dataInicio:{
+        dataInicio: {
           gte: new Date(year ? year : new Date().getFullYear()),
           lt: new Date(year ? year : new Date().getFullYear(), 12, 31),
-        }
+        },
       },
       select: {
         total: true,
@@ -190,39 +190,60 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
         dataInicio: "asc",
       },
     });
-  
+
     // Lista de todos os meses do ano
     const meses = [
-      "jan.", "fev.", "mar.", "abr.", "mai.", "jun.",
-      "jul.", "ago.", "set.", "out.", "nov.", "dez."
+      "jan.",
+      "fev.",
+      "mar.",
+      "abr.",
+      "mai.",
+      "jun.",
+      "jul.",
+      "ago.",
+      "set.",
+      "out.",
+      "nov.",
+      "dez.",
     ];
-  
+
     // Inicializa o acumulador para todos os meses
-    const result = meses.reduce((acc, mes) => {
-      acc[mes] = {
-        month: `${mes}`,
-        count: 0,
-        total: 0,
-        trafego: {
-          google: 0,
-          tiktok: 0,
-          facebook: 0,
-          instagram: 0,
-          outros: 0
+    const result = meses.reduce(
+      (acc, mes) => {
+        acc[mes] = {
+          month: `${mes}`,
+          count: 0,
+          total: 0,
+          trafego: {
+            google: 0,
+            tiktok: 0,
+            facebook: 0,
+            instagram: 0,
+            outros: 0,
+          },
+        };
+        return acc;
+      },
+      {} as Record<
+        string,
+        {
+          month: string;
+          count: number;
+          total: number;
+          trafego: {
+            google: number;
+            tiktok: number;
+            facebook: number;
+            instagram: number;
+            outros: number;
+          };
         }
-      };
-      return acc;
-    }, {} as Record<string, { month: string; count: number; total: number, trafego: {
-      google: number,
-      tiktok: number,
-      facebook: number,
-      instagram: number,
-      outros: number
-    } }>);
-  
+      >
+    );
+
     // Inicializa o total absoluto
     const totalAbsoluto = {
-      month: 'Total Absoluto',
+      month: "Total Absoluto",
       count: 0,
       total: 0,
       trafego: {
@@ -230,39 +251,41 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
         tiktok: 0,
         facebook: 0,
         instagram: 0,
-        outros: 0
-      }
+        outros: 0,
+      },
     };
-  
+
     // Percorre os orçamentos e acumula os valores
-    orcamentos.forEach(orcamento => {
-      const month = new Date(orcamento.dataInicio).toLocaleString("pt-BR", {
-        month: "short",
-      }).toLowerCase();  // Nome do mês em minúsculas para coincidir com a lista
-  
+    orcamentos.forEach((orcamento) => {
+      const month = new Date(orcamento.dataInicio)
+        .toLocaleString("pt-BR", {
+          month: "short",
+        })
+        .toLowerCase(); // Nome do mês em minúsculas para coincidir com a lista
+
       // Atualiza os valores acumulados para o mês correspondente
       result[month].count += 1;
       result[month].total += orcamento.total;
-  
+
       // Atualiza os totais absolutos
       totalAbsoluto.count += 1;
       totalAbsoluto.total += orcamento.total;
-  
+
       // Atualiza a contagem de tráfego com base no canal
       switch (orcamento.trafegoCanal.toLowerCase()) {
-        case 'google':
+        case "google":
           result[month].trafego.google += 1;
           totalAbsoluto.trafego.google += 1;
           break;
-        case 'titok':
+        case "titok":
           result[month].trafego.tiktok += 1;
           totalAbsoluto.trafego.tiktok += 1;
           break;
-        case 'facebook':
+        case "facebook":
           result[month].trafego.facebook += 1;
           totalAbsoluto.trafego.facebook += 1;
           break;
-        case 'instagram':
+        case "instagram":
           result[month].trafego.instagram += 1;
           totalAbsoluto.trafego.instagram += 1;
           break;
@@ -272,13 +295,13 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
           break;
       }
     });
-  
+
     // Converte o objeto acumulado em um array
     const resultArray = Object.values(result);
-  
+
     // Adiciona o total absoluto ao final do array
     resultArray.push(totalAbsoluto);
-  
+
     return resultArray;
-  } 
+  }
 }
