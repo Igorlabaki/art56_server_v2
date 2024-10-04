de6ca18ef0982fb119c6c4676eb0dba376d2762d
Igor Goncalo <igorlabakig@gmail.com>
2024-10-04 11:09:44 +0100

    s

--- src/repository/inPrisma/prismaOrcamentoRepository.ts
+++ src/repository/inPrisma/prismaOrcamentoRepository.ts
@@ -3,9 +3,9 @@ import {
   IOrcamentoRepository,
   ListOrcamentoParams,
   UpdateOrcamentoParams,
-} from '../IOrcamentoRepository';
+} from "../IOrcamentoRepository";
 
-import { PrismaClient, Orcamento } from '@prisma/client';
+import { PrismaClient, Orcamento } from "@prisma/client";
 
 export class PrismaOrcamentoRepository implements IOrcamentoRepository {
   constructor(private readonly prisma: PrismaClient) {}
@@ -34,7 +34,10 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
     });
   }
 
-  async update({ data, orcamentoId }: UpdateOrcamentoParams): Promise<Orcamento | null> {
+  async update({
+    data,
+    orcamentoId,
+  }: UpdateOrcamentoParams): Promise<Orcamento | null> {
     return await this.prisma.orcamento.update({
       where: {
         id: orcamentoId,
@@ -45,136 +48,153 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
     });
   }
 
-  async list({query, month, year,take}:ListOrcamentoParams): Promise<Orcamento[]> {
-      return await this.prisma.orcamento.findMany({
-        where:{
-          ...(query && {
-            nome: {
-              contains: query
-            }
-          }),
-          ...(month ? {
-              dataInicio: {
-                gte: new Date(year ? year : new Date().getFullYear(), month - 1, 1), // Início do mês
-                lt: new Date(year ? year : new Date().getFullYear(), month, 1),
-              }
-              } : {
-                dataInicio: {
-                  gte: new Date(year ? year : new Date().getFullYear()),
-                  lt: new Date(year ? year : new Date().getFullYear(), 12, 31),
-                }
-              }
-            ),
-          aprovadoAr756: false,
-          aprovadoCliente: false
+  async list({
+    query,
+    month,
+    year,
+    take,
+  }: ListOrcamentoParams): Promise<Orcamento[]> {
+    return await this.prisma.orcamento.findMany({
+      where: {
+        ...(query && {
+          nome: {
+            contains: query,
           },
-          ...(take && { take : Number(take) }),
-          orderBy:{
-            dataInicio: "asc"
-          }
-      });
-    }
-
-  async listAprovado({query, month, year}:ListOrcamentoParams): Promise<Orcamento[]> {
-      return await this.prisma.orcamento.findMany({
-        where:{
-          ...(query && {
-            nome: {
-              contains: query
-            }
-          }),
-          ...(month ? {
+        }),
+        ...(month
+          ? {
               dataInicio: {
-                gte: new Date(year ? year : new Date().getFullYear(), month - 1, 1), // Início do mês
+                gte: new Date(
+                  year ? year : new Date().getFullYear(),
+                  month - 1,
+                  1
+                ), // Início do mês
                 lt: new Date(year ? year : new Date().getFullYear(), month, 1),
-              }
-              } : {
-                dataInicio: {
-                  gte: new Date(year ? year : new Date().getFullYear()),
-                  lt: new Date(year ? year : new Date().getFullYear(), 12, 31),
-                }
-              }
-            ),
-          aprovadoAr756: true,
-          aprovadoCliente: true
-          },
-          orderBy:{
-            dataInicio: "asc"
-          }
-      });
-    }
-
-    async trafegoCount(): Promise<any> {
-      const trafegoCounts = await this.prisma.orcamento.groupBy({
-        by: ['trafegoCanal'],
-        _count: {
-          trafegoCanal: true,
-        },
-      });
-    
-      const orcamentoCount = trafegoCounts.reduce((acc, curr) => acc + curr._count.trafegoCanal, 0);
-    
-      const trafegoData = {
-        todos: orcamentoCount,
-        google: trafegoCounts.find(item => item.trafegoCanal === 'Google')?._count.trafegoCanal || 0,
-        instagram: trafegoCounts.find(item => item.trafegoCanal === 'Instagram')?._count.trafegoCanal || 0,
-        tikTok: trafegoCounts.find(item => item.trafegoCanal === 'TiTok')?._count.trafegoCanal || 0,
-        facebook: trafegoCounts.find(item => item.trafegoCanal === 'Facebook')?._count.trafegoCanal || 0,
-        amigos: trafegoCounts.find(item => item.trafegoCanal === 'Amigos')?._count.trafegoCanal || 0,
-        outros: trafegoCounts.find(item => item.trafegoCanal === 'Outros')?._count.trafegoCanal || 0,
-      };
-    
-      return trafegoData;
-    }
-
-    async monthCount(): Promise<any> {
-      const orcamentosPorMes = await this.prisma.$queryRaw`
-      SELECT 
-        MONTH(created_at) AS month_number,
-        COUNT(*) AS orcamentos_count
-      FROM 
-        orcamento
-      WHERE 
-        created_at >= ${new Date(new Date().getFullYear(), 0, 1)} 
-        AND created_at < ${new Date(new Date().getFullYear() + 1, 0, 1)}
-      GROUP BY 
-        month_number
-      ORDER BY 
-        month_number;  -- Mantenha a ordenação pelo número do mês
-    `;
-
-    return {orcamentosPorMes}
-    
-      /* const orcamentos = await this.prisma.orcamento.findMany({
-        where:{
-          aprovadoAr756: true,
-          aprovadoCliente: true,
-        },
-        select: {
-          dataInicio: true,
-          total: true,
-        },
-      });
-    
-      const result = orcamentos.reduce((acc, orcamento) => {
-        const month = new Date(orcamento.dataInicio).toLocaleString('pt-BR', {
-          month: 'long',
-          year: 'numeric',
-        });
-        
-        // Verifica se o mês já existe no acumulador
-        if (!acc[month]) {
-          acc[month] = { month: month, count: 0, total: 0 };
-        }
-        
-        // Atualiza os valores acumulados
-        acc[month].count += 1; // conta os orçamentos
-        acc[month].total += orcamento.total; // soma os totais
-    
-        return acc;
-      }, {} as Record<string, { month: string; count: number; total: number }>);
-    
-      // Converte o objeto acumulado em um array
-      return Object.values(result);*/
-    } 
+              },
+            }
+          : {
+              dataInicio: {
+                gte: new Date(year ? year : new Date().getFullYear()),
+                lt: new Date(year ? year : new Date().getFullYear(), 12, 31),
+              },
+            }),
+        aprovadoAr756: false,
+        aprovadoCliente: false,
+      },
+      ...(take && { take: Number(take) }),
+      orderBy: {
+        dataInicio: "asc",
+      },
+    });
   }
+
+  async listAprovado({
+    query,
+    month,
+    year,
+  }: ListOrcamentoParams): Promise<Orcamento[]> {
+    return await this.prisma.orcamento.findMany({
+      where: {
+        ...(query && {
+          nome: {
+            contains: query,
+          },
+        }),
+        ...(month
+          ? {
+              dataInicio: {
+                gte: new Date(
+                  year ? year : new Date().getFullYear(),
+                  month - 1,
+                  1
+                ), // Início do mês
+                lt: new Date(year ? year : new Date().getFullYear(), month, 1),
+              },
+            }
+          : {
+              dataInicio: {
+                gte: new Date(year ? year : new Date().getFullYear()),
+                lt: new Date(year ? year : new Date().getFullYear(), 12, 31),
+              },
+            }),
+        aprovadoAr756: true,
+        aprovadoCliente: true,
+      },
+      orderBy: {
+        dataInicio: "asc",
+      },
+    });
+  }
+
+  async trafegoCount(): Promise<any> {
+    const trafegoCounts = await this.prisma.orcamento.groupBy({
+      by: ["trafegoCanal"],
+      _count: {
+        trafegoCanal: true,
+      },
+    });
+
+    const orcamentoCount = trafegoCounts.reduce(
+      (acc, curr) => acc + curr._count.trafegoCanal,
+      0
+    );
+
+    const trafegoData = {
+      todos: orcamentoCount,
+      google:
+        trafegoCounts.find((item) => item.trafegoCanal === "Google")?._count
+          .trafegoCanal || 0,
+      instagram:
+        trafegoCounts.find((item) => item.trafegoCanal === "Instagram")?._count
+          .trafegoCanal || 0,
+      tikTok:
+        trafegoCounts.find((item) => item.trafegoCanal === "TiTok")?._count
+          .trafegoCanal || 0,
+      facebook:
+        trafegoCounts.find((item) => item.trafegoCanal === "Facebook")?._count
+          .trafegoCanal || 0,
+      amigos:
+        trafegoCounts.find((item) => item.trafegoCanal === "Amigos")?._count
+          .trafegoCanal || 0,
+      outros:
+        trafegoCounts.find((item) => item.trafegoCanal === "Outros")?._count
+          .trafegoCanal || 0,
+    };
+
+    return trafegoData;
+  }
+
+  async monthCount(): Promise<any> {
+    const orcamentos = await this.prisma.orcamento.findMany({
+      where: {
+        aprovadoAr756: true,
+        aprovadoCliente: true,
+      },
+      select: {
+        dataInicio: true,
+        total: true,
+      },
+    });
+
+    const result = orcamentos.reduce((acc, orcamento) => {
+      const month = new Date(orcamento.dataInicio).toLocaleString("pt-BR", {
+        month: "long",
+        year: "numeric",
+      });
+
+      // Verifica se o mês já existe no acumulador
+      if (!acc[month]) {
+        acc[month] = { month: month, count: 0, total: 0 };
+      }
+
+      // Atualiza os valores acumulados
+      acc[month].count += 1; // conta os orçamentos
+      acc[month].total += orcamento.total; // soma os totais
+
+      return acc;
+    }, {} as Record<string, { month: string; count: number; total: number }>);
+
+    // Converte o objeto acumulado em um array
+    return Object.values(result);
+  }
+}
