10272d1ad67b58728a92730aae77435ffab12c11
Igor Goncalo <igorlabakig@gmail.com>
2024-10-08 08:53:47 +0100

    s

--- src/repository/inPrisma/prismaOrcamentoRepository.ts
+++ src/repository/inPrisma/prismaOrcamentoRepository.ts
@@ -180,7 +180,7 @@ export class PrismaOrcamentoRepository implements IOrcamentoRepository {
         b.value / trafegoData?.todos - a.value / trafegoData?.todos
     );
 
-    return {trafegoData, sortedSources};
+    return {todos:trafegoData.todos, sortedSources};
   }
 
   async monthCount({ year }: MonthCountParams): Promise<any> {
