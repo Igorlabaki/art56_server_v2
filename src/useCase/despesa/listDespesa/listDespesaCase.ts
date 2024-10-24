0a95d8df4b05afc823ae331d3a3596d305eb6a9a
Igor Goncalo <igorlabakig@gmail.com>
2024-10-24 11:57:52 +0100

    e

--- src/useCase/despesa/listDespesa/listDespesaCase.ts
+++ src/useCase/despesa/listDespesa/listDespesaCase.ts
@@ -1,13 +1,49 @@
-import { IDespesaRepository, IListByCategoriaDespesasParams } from "../../../repository/IDespesaRepository";
+import { Despesa } from "@prisma/client";
+import {
+  IDespesaRepository,
+  IListByCategoriaDespesasParams,
+} from "../../../repository/IDespesaRepository";
 
+interface DespesaListState {
+  recorrentes: {
+    total: number;
+    list: Despesa[];
+  };
+  naoRecorrentes: {
+    total: number;
+    list: Despesa[];
+  };
+}
 class ListDespesaCase {
   constructor(private despesaRepository: IDespesaRepository) {}
 
   async execute(query: IListByCategoriaDespesasParams) {
     const despesaList = await this.despesaRepository.list(query);
-    
 
-    return despesaList;
+    const despesaListState : DespesaListState = {
+      recorrentes: {
+        total: 0,
+        list: [],
+      },
+      naoRecorrentes: {
+        total: 0,
+        list: [],
+      },
+    };
+
+    if (despesaList) {
+      for (const despesa of despesaList) {
+        if (despesa.recorrente) {
+          despesaListState.recorrentes.total += despesa.valor;
+          despesaListState.recorrentes.list.push(despesa);
+        } else {
+          despesaListState.naoRecorrentes.total += despesa.valor;
+          despesaListState.naoRecorrentes.list.push(despesa);
+        }
+      }
+    }
+
+    return despesaListState;
   }
 }
 
