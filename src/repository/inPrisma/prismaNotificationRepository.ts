855e41b4d62997e52eaa0a70b76d897c01f521a5
Igor Goncalo <igorlabakig@gmail.com>
2024-10-28 14:38:51 +0000

    update notification list query

--- src/repository/inPrisma/prismaNotificationRepository.ts
+++ src/repository/inPrisma/prismaNotificationRepository.ts
@@ -49,9 +49,16 @@ export class PrismaNotificationRepository implements INotificationRepository {
   }
 
   async list(reference: string | undefined): Promise<Notification[]> {
+    const today = new Date();
     return await this.prisma.notification.findMany({
+      where: {
+        createdAt: {
+          gte:  new Date(today.setHours(0, 0, 0, 0)), // maior ou igual a início do dia
+          lte:  new Date(today.setHours(23, 59, 59, 999)),   // menor ou igual a fim do dia
+        },
+      },
       orderBy: {
-        createdAt: "asc",
+        createdAt: "desc",
       },
     });
   }
